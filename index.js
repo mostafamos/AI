ea1f4a1943d475e9d1fd58e50d6ee90a515493d3
Mostafa <mostafa@MacBook-Air-2.local>
2023-01-01 11:20:22 +0900

    node

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+  const {text} = req.body
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${text}`,
+    temperature: 0.7,
+    max_tokens: 50,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  })
+  //console.log(response.data)
+  if (response.data.choices[0].text){
+    res.json({
+      message: response.data.choices[0].text
+    })
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
