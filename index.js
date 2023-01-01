23df2ca7dedc619cec2a31832cd998386507e513
Mostafa <mostafa@MacBook-Air-2.local>
2023-01-01 12:54:21 +0900

    rm dir

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.post('/', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server listening on port 3000');
 });
